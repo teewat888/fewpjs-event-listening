c69fb0a0a97261e68238f97723a31b7e0fbef8dc
teerawat prarom <teewat@yahoo.com>
2021-08-01 00:33:45 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click',function(e){
+        alert('I was clicked');
+    })
 }
+addingEventListener();
